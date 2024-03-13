7cd4ef7279382fec7c9f51115313a372a6216241
Mariane Jankoski <110405094+mariane12022007@users.noreply.github.com>
2024-03-13 11:46:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
 
-contadores[0]
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor (tempoFinal / 1000);
+    return segundos;
+}
+
+contadores[0].textContent="contagem regressiva";
 
 for (let i=0;i < botões.length; i++) {
     console.log(i);
